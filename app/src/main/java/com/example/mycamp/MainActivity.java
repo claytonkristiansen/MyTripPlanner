c0ccf539e79ee92798d992b12c3313bc286c72eb
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2020-10-09 01:01:23 -0500

    update

--- app/src/main/java/com/example/mycamp/MainActivity.java
+++ app/src/main/java/com/example/mycamp/MainActivity.java
@@ -8,12 +8,34 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.JsonElement;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.MobileServiceException;
+import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceJsonTable;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import com.google.gson.*;
+import java.net.*;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.val;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,6 +45,10 @@ public class MainActivity extends AppCompatActivity
     private TextView txt;
     private Integer number = 0;
     private Date date = new Date();
+    Gson gsonBuilder = new Gson();
+
+    URL databaseUrl;
+    HttpURLConnection databaseConnection;
 
     //RecyclerView stuff
     private RecyclerView recommendationsList;
@@ -40,7 +66,13 @@ public class MainActivity extends AppCompatActivity
     };
 
     MobileServiceClient msClient;
-    MobileServiceTable<Trip>
+    MobileServiceTable<Trip> msTable;
+    List<Trip> results;
+
+
+
+    public MainActivity() throws MalformedURLException {
+    }
 
     class UpDateUITask extends AsyncTask<View, String, String>
     {
@@ -63,6 +95,90 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    class NetworkTask extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            networkTaskArrayList.add(this);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids)
+        {
+
+            try {
+                results = msTable.execute().get();
+
+                //Offline Sync
+                //final List<ToDoItem> results = refreshItemsFromMobileServiceTableSyncTable();
+
+            } catch (final Exception e){
+
+            }
+
+
+
+
+            //try {
+
+
+
+
+//                String tableName = "Trips";
+//                databaseUrl = new URL("https://mytripplannerappservice.azurewebsites.net/api/" + tableName + "/");
+//                databaseConnection = (HttpURLConnection) databaseUrl.openConnection();
+//                databaseConnection.setRequestMethod("POST");
+//                databaseConnection.setDoOutput(true);
+//                databaseConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+//
+//
+//
+//                Trip testTrip = new Trip(0, "FROM ANDROID APP", "8-24-2020", "8-30-2020");
+//
+//                String tripJSON = gsonBuilder.toJson(testTrip);
+//                Trip tempTrip = gsonBuilder.fromJson(tripJSON, Trip.class);
+//
+//
+//
+//                OutputStream o = databaseConnection.getOutputStream();
+//
+//                DataOutputStream out = new DataOutputStream(o);56
+//                out.write(tripJSON.getBytes());
+//                out.flush();
+//                out.close();
+//
+//                BufferedReader reponse = new BufferedReader(new InputStreamReader(databaseConnection.getInputStream()));
+//
+//                String inputLine;
+//                StringBuffer responseString = new StringBuffer();
+//
+//                while ((inputLine = reponse.readLine()) != null) {
+//                    responseString.append(inputLine);
+//                }
+//
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            super.onPostExecute(aVoid);
+            networkTaskArrayList.remove(this);
+            number++;
+        }
+
+    }
+
+    ArrayList<NetworkTask> networkTaskArrayList = new ArrayList<NetworkTask>();
+
+    NetworkTask tempTask;// = new NetworkTask();
+
     private UpDateUITask myTask = new UpDateUITask();
 
     @Override
@@ -76,13 +192,35 @@ public class MainActivity extends AppCompatActivity
         txt = ((TextView) findViewById(R.id.dateText));
         txt.setText("food");
 
-        try {
-            msClient = new MobileServiceClient(
-                    "https://mytripplanner.azurewebsites.net",
-                    this);
-        } catch (MalformedURLException e) {
+
+        try
+        {
+            msClient = new MobileServiceClient("https://mytripplanner.azurewebsites.net", this);
+
+            msClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
+                @Override
+                public OkHttpClient createOkHttpClient() {
+                    OkHttpClient client = new OkHttpClient.Builder()
+                            .connectTimeout(20, TimeUnit.SECONDS)
+                            .readTimeout(20, TimeUnit.SECONDS)
+                            .build();
+
+                    return client;
+                }
+            });
+
+            msTable = msClient.getTable(Trip.class);
+
+            Trip tempTrip = new Trip("Shi Shi", "10-10-2021", "10-20-2021", "Washington");
+            msTable.insert(tempTrip);
+
+
+        }
+        catch (MalformedURLException e)
+        {
             e.printStackTrace();
         }
+
 
 //        recommendationsList = (RecyclerView) findViewById(R.id.my_recycler_view);
 //        recListLayoutManager = new LinearLayoutManager(this);
@@ -90,7 +228,10 @@ public class MainActivity extends AppCompatActivity
 //        recListAdapter = new MyHikingTrailAdapter(dataSet);
 //        recommendationsList.setAdapter(recListAdapter);
 
-        myTask.execute(findViewById(R.id.dateText));
+
+        //myNetworkTask.execute();
+
+        //myTask.execute(findViewById(R.id.dateText));
     }
 
     private void updateUI(View view)
@@ -136,6 +277,13 @@ public class MainActivity extends AppCompatActivity
         {
             timeToggle = false;
         }
+        NetworkTask myTask = new NetworkTask();
+        //networkTaskArrayList.add(myTask);
+        myTask.execute();
+
+        Integer i = networkTaskArrayList.size();
+
+        ((Button)view).setText(i.toString());
 
 
 
--- app/src/main/java/com/example/mycamp/Trip.java
+++ app/src/main/java/com/example/mycamp/Trip.java
@@ -1,4 +1,99 @@
 package com.example.mycamp;
 
-public class Trip {
+public class Trip
+{
+    @com.google.gson.annotations.SerializedName("id")
+    private String m_tripId;
+
+    @com.google.gson.annotations.SerializedName("name")
+    private String m_name;
+
+    @com.google.gson.annotations.SerializedName("complete")
+    private boolean m_complete;
+
+    @com.google.gson.annotations.SerializedName("startDate")
+    private String m_startDate;
+
+    @com.google.gson.annotations.SerializedName("endDate")
+    private String m_endDate;
+
+    @com.google.gson.annotations.SerializedName("location")
+    private String m_location;
+
+//    @com.google.gson.annotations.SerializedName("EndDate")
+//    private String m_endDate;
+
+
+
+    public Trip(String tripId, String name, String startDate, String endDate, String location)
+    {
+        m_tripId = tripId;
+        m_name = name;
+        m_complete = false;
+        m_startDate = startDate;
+        m_endDate = endDate;
+        m_location = location;
+    }
+
+    public Trip(String name, String startDate, String endDate, String location)
+    {
+        m_name = name;
+        m_complete = false;
+        m_startDate = startDate;
+        m_endDate = endDate;
+        m_location = location;
+    }
+
+    public Trip() {}
+
+    public String getName() {
+        return m_name;
+    }
+
+    /**
+     * Sets the item text
+     *
+     * @param name
+     *            text to set
+     */
+    public final void setText(String name) {
+        m_name = name;
+    }
+
+    /**
+     * Returns the item id
+     */
+    public String getId() {
+        return m_tripId;
+    }
+
+    /**
+     * Sets the item id
+     *
+     * @param id
+     *            id to set
+     */
+    public final void setId(String id) {
+        m_tripId = id;
+    }
+
+    /**
+     * Indicates if the item is marked as completed
+     */
+    public boolean isComplete() {
+        return m_complete;
+    }
+
+    /**
+     * Marks the item as completed or incompleted
+     */
+    public void setComplete(boolean complete) {
+        m_complete = complete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return  ((Trip) o).m_tripId == m_tripId;
+    }
+
 }
